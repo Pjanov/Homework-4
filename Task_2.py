a6b38ae5a990ab5886791ec067adad144845ba9e
Андрей <pjanov84@mail.ru>
2022-10-21 10:15:08 +0300

    Task_2 Solution

--- Task_2.py
+++ Task_2.py
@@ -4,6 +4,17 @@
 # simple_number(147420) => [2, 3, 5, 7, 13];
 # simple_number(374220) => [2, 3, 5, 7, 11];
 
+def list_of_multipliers(val):
+    my_list = []
+    n = 2
+    while val >= n:
+        if val % n == 0:
+            my_list.append(n)
+            val /= n
+        else:
+            n += 1
+    return set(my_list)
 
 
-
+simple_number = 1474200
+print(f'simple_number({simple_number}) =>', list(list_of_multipliers(simple_number)))
